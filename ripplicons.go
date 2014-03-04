6e199393f0c2d59e1ce984cf107df02d148ffbbd
Luke Cyca <me@lukecyca.com>
2014-03-04 09:13:04 -0800

    Decode ripple addresses into []byte suitable for sigil

---
+++ ripplicons.go
@@ -0,0 +1,61 @@
+package ripplicons
+
+import (
+	"bytes"
+	"math"
+)
+
+func toBytes(smallBase int, digits []int) []byte {
+	// Takes an int slice of arbitary base smallBase,
+	// converts it to base-256, and returns it as a byte slice
+
+	n := make([]int, len(digits)) // least-sig first
+
+	for i := 0; i < len(digits); i++ {
+		for j := 0; j < len(n); j++ {
+			n[j] *= smallBase
+		}
+
+		n[0] += digits[i]
+
+		normalizeBase(256, n)
+	}
+
+	// Reverse n and convert to bytes
+	out := make([]byte, len(digits)+1)
+	for i := 0; i < len(n); i++ {
+		out[i+1] = byte(n[len(n)-i-1])
+	}
+
+	return out
+}
+
+func normalizeBase(b int, digits []int) {
+	for i := 0; i < len(digits); i++ {
+		if digits[i] > b {
+			digits[i+1] = digits[i+1] + int(math.Floor(float64(digits[i])/float64(b)))
+			digits[i] = digits[i] % b
+		}
+	}
+}
+
+func Decode(address string) []byte {
+	// Takes a ripple address and returns an MD5 sum suitable for
+	// use with github.com/cupcake/sigil
+
+	alphabet := []byte("rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz")
+	base58digits := []int{}
+
+	for _, r := range address {
+		idx := bytes.IndexByte(alphabet, byte(r))
+		base58digits = append(base58digits, idx)
+	}
+
+	bytes := toBytes(58, base58digits)
+
+	// Trim off the last 4 (checksum) bytes and extra leading zeros
+	// to end up with exactly 21 bytes
+	bytes = bytes[len(bytes)-25 : len(bytes)-4]
+
+	return bytes
+}
---
+++ ripplicons_test.go
@@ -0,0 +1,42 @@
+package ripplicons
+
+import (
+	"crypto/md5"
+	"fmt"
+	"testing"
+)
+
+/* This is a table of common ripple addresses, and the md5 representation
+   as given by singpolyma's implementation:
+   http://userscripts.org/scripts/show/350423
+*/
+
+var decodeMD5Tests = []struct {
+	address  string // ripple address
+	expected string // expected md5 hex
+}{
+	// Zero and One addresses
+	{"rrrrrrrrrrrrrrrrrrrrrhoLvTp", "2319ac34f4848755a639fd524038dfd3"},
+	{"rrrrrrrrrrrrrrrrrrrrBZbvji", "0f7173e963614f142641d4699cc329d8"},
+
+	// Common gateway issuing addresses
+	{"rNPRNzBB92BVpAhhZr4iXDTveCgV5Pofm9", "90f13b28688fedbaeb0c2a87e60b0618"},
+	{"rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B", "2d6e14d2929bd0389784c269660f18d7"},
+	{"rfYv1TXnwgDDK4WQNbFALykYuEBnrR4pDX", "bbcf11f51a123d9a673f4d49bb0e56fc"},
+	{"rGwUWgN5BEg3QGNY3RX2HfYowjUTZdid3E", "c516884bc476752c209dd827fc7df6f9"},
+	{"rnuF96W4SZoCJmbHYBFoJZpR8eCaxNvekK", "be57f17c6ceb71834563f955bc848982"},
+	{"r3ADD8kXSUKHd6zTCKfnKT3zV9EZHjzp1S", "7584715a37f688d590027bf8b9db07fe"},
+	{"rLEsXccBGNR3UPuPu2hUXPjziKC3qKSBun", "1f3fee5f9b82181a4413c30cf7fc8aa5"},
+}
+
+func TestDecodeMD5(t *testing.T) {
+	for _, r := range decodeMD5Tests {
+		bytes := md5.Sum(Decode(r.address))
+		if fmt.Sprintf("%x", bytes) != r.expected {
+			t.Errorf(
+				"For %s got %x, expected %s",
+				r.address, bytes, r.expected,
+			)
+		}
+	}
+}
