55fe2abea5b4833d0935f341ec4434f8d80b7412
Luke Cyca <me@lukecyca.com>
2014-03-04 12:22:48 -0800

    Better godoc

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Sigil-based identicons for Ripple addresses
 
 [![Build Status](https://drone.io/github.com/lukecyca/ripplicons/status.png)](https://drone.io/github.com/lukecyca/ripplicons/latest)
 
-Based off @singpolyma's GreaseMonkey script: http://userscripts.org/scripts/show/350423
+http://godoc.org/github.com/lukecyca/ripplicons
--- ripplicons.go
+++ ripplicons.go
@@ -1,3 +1,8 @@
+/*
+Ripplicons decodes a human-readable ripple address into salient bytes suitable for creating
+a Sigil-based identicon (https://github.com/cupcake/sigil). The idea was originally
+implemented by singpolyma as a Greasemonkey script (http://userscripts.org/scripts/show/350423).
+*/
 package ripplicons
 
 import (
@@ -5,9 +10,9 @@ import (
 	"math"
 )
 
+// toBytes accepts a []int of arbitary base smallBase,
+// converts it to base-256, and returns it as a []byte
 func toBytes(smallBase int, digits []int) []byte {
-	// Takes an int slice of arbitary base smallBase,
-	// converts it to base-256, and returns it as a byte slice
 
 	n := make([]int, len(digits)) // least-sig first
 
@@ -39,10 +44,9 @@ func normalizeBase(b int, digits []int) {
 	}
 }
 
+// Decode accepts a ripple address and returns a []byte suitable for
+// use with https://github.com/cupcake/sigil.
 func Decode(address string) []byte {
-	// Takes a ripple address and returns a []byte suitable for
-	// use with github.com/cupcake/sigil
-
 	alphabet := []byte("rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz")
 	base58digits := []int{}
 
